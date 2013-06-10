93a7bb90a16ebb973aa50ec6462db5283cc98a03
Gaurav Ramanan <gaurav@rcorp.co.in>
2013-06-10 12:15:36 +0530

    Fix: Scrollbar fix in all pages

--- investmentopportunity.php
+++ investmentopportunity.php
@@ -3,128 +3,149 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<div id="contentWrapper">
-			<div>
+		<div is="fb-root">
+			<div id="contentWrapper">
 				<div>
-					<img src="tab-header-3.jpg" />
-				</div>
-				<div class="btn">
 					<div>
-						<a href="index.php">Home</a>
-				    </div>
-				    <div>
-						<a href="tabs/Country Profile  1st Page.docx">Our Country</a>
-				    </div>
-				    <div>
-				    	<a href="investmentopportunity.php">Investment Opportunity</a>
+						<img src="tab-header-3.jpg" />
 					</div>
-					<div>
-						<a href="visaform.php">Visa Form</a>
+					<div class="btn">
+						<div>
+							<a href="index.php">Home</a>
+					    </div>
+					    <div>
+							<a href="tabs/Country Profile  1st Page.docx">Our Country</a>
+					    </div>
+					    <div>
+					    	<a href="investmentopportunity.php">Investment Opportunity</a>
+						</div>
+						<div>
+							<a href="visaform.php">Visa Form</a>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="gallery">
-				<p>Why to Invest in Burkina Faso?</p>
-			</div>
-			<div class="invest">
-					<div class = "investcontent">
-						<p id="investcontent">1. A political and institutional stability based on an ongoing quest for social dialogue and political consensus.</p>
-					</div>
-					
-					<div class = "investcontent">
-						<p id="investcontent">2. A stable and sustainable healthy macroeconomic context.</p>
-					</div>
+				<div class="gallery">
+					<p>Why to Invest in Burkina Faso?</p>
+				</div>
+				<div class="invest">
+						<div class = "investcontent">
+							<p id="investcontent">1. A political and institutional stability based on an ongoing quest for social dialogue and political consensus.</p>
+						</div>
+						
+						<div class = "investcontent">
+							<p id="investcontent">2. A stable and sustainable healthy macroeconomic context.</p>
+						</div>
 
-					<div class = "investcontent">
-						<p id="investcontent">3. A firm choice for a free-market economy opened to foreign investments.</p>
-					</div>
+						<div class = "investcontent">
+							<p id="investcontent">3. A firm choice for a free-market economy opened to foreign investments.</p>
+						</div>
 
-					<div class = "investcontent">
-						<p id="investcontent">4. A favourable, innovative and especially attractive legal and regulatory framework, with a firm policy relating to the protection, the security and the promotion of investments.</p>
-					</div>
+						<div class = "investcontent">
+							<p id="investcontent">4. A favourable, innovative and especially attractive legal and regulatory framework, with a firm policy relating to the protection, the security and the promotion of investments.</p>
+						</div>
 
-					<div class = "investcontent">
-						<p id="investcontent">5. An area of monetary stability, with a common currency within UEMOA, freely convertible with a fixed parity in relation to the Euro.</p>
-					</div>
+						<div class = "investcontent">
+							<p id="investcontent">5. An area of monetary stability, with a common currency within UEMOA, freely convertible with a fixed parity in relation to the Euro.</p>
+						</div>
 
-					<div class = "investcontent">
-						<p id="investcontent">6. A young, dynamic, enterprising population, as well as an abundant and hard-working workforce.</p>
-					</div>
+						<div class = "investcontent">
+							<p id="investcontent">6. A young, dynamic, enterprising population, as well as an abundant and hard-working workforce.</p>
+						</div>
+						
+						<div class = "investcontent">
+							<p id="investcontent">7. A central geographical location in the heart of West Africa, ECOWAS and UEMOA states. A common border with six States of ECOWAS and five States of UEMOA. Such a position enables the country to have access to a potential market of 73 million inhabitants from UEMOA States and over 280 million inhabitants in ECOWAS States.</p>
+						</div>
+						
+						<div class = "investcontent">
+							<p id="investcontent">8. A key legal and judicial environment, a harmonised business law that is continuously improved within the framework of the Organisation for the Harmonization of Business Law in Africa (OHADA).</p>
+						</div>
+						
+						<div class = "investcontent">
+							<p id="investcontent">9. A good international road network, with a performing international transit system which enables Burkina Faso to be a hub for UEMOA and ECOWAS states.</p>
+						</div>
 					
-					<div class = "investcontent">
-						<p id="investcontent">7. A central geographical location in the heart of West Africa, ECOWAS and UEMOA states. A common border with six States of ECOWAS and five States of UEMOA. Such a position enables the country to have access to a potential market of 73 million inhabitants from UEMOA States and over 280 million inhabitants in ECOWAS States.</p>
-					</div>
+						<div class = "investcontent">
+							<p id="investcontent">10. Significant economic potential which is not yet sufficiently used in the areas of agriculture, mining, industries, tourism and services.</p>
+						</div>
+									
 					
-					<div class = "investcontent">
-						<p id="investcontent">8. A key legal and judicial environment, a harmonised business law that is continuously improved within the framework of the Organisation for the Harmonization of Business Law in Africa (OHADA).</p>
-					</div>
-					
-					<div class = "investcontent">
-						<p id="investcontent">9. A good international road network, with a performing international transit system which enables Burkina Faso to be a hub for UEMOA and ECOWAS states.</p>
 					</div>
 				
-					<div class = "investcontent">
-						<p id="investcontent">10. Significant economic potential which is not yet sufficiently used in the areas of agriculture, mining, industries, tourism and services.</p>
+				<div class="regstration">
+					<p>Sectors where you can invest.</p>
+				</div>
+				
+				<div class="investgallery">
+					<div class="gallerycell">
+						<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg1.jpg"></a></div>					
+						<div class="cell2">
+							Animal Husbandary
+						</div>		
 					</div>
-								
+
+					<div class="gallerycell">
+						<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg2.jpg"></a></div>					
+						<div class="cell2">
+							Agriculture
+						</div>		
+					</div>
+
+					<div class="gallerycell">
+						<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg4.jpg"></a></div>					
+						<div class="cell2">
+							Quarrying
+						</div>		
+					</div>
+					
+					<div class="gallerycell">
+						<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg3.jpg"></a></div>					
+						<div class="cell2">
+							Gold Mining
+						</div>		
+					</div>
+
+					<div class="gallerycell">
+						<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg5.jpg"></a></div>					
+						<div class="cell2">
+							Real Estate
+						</div>		
+					</div>
+
+					<div class="gallerycell">
+						<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg6.jpg"></a></div>					
+						<div class="cell2">
+							Industry
+						</div>		
+					</div>
+				</div>
+				<div style="clear:both;"></div>			
 				
+				<div style="background: #006637;height: 31px;">
+					<p style="float: right;font-size: 19px;color: white;margin-right: 7px;">Designed by Ramanan Corporation</p>
 				</div>
-			
-			<div class="regstration">
-				<p>Sectors where you can invest.</p>
+				<a href = "http://www.facebook.com/dialog/pagetab?app_id=397865700321865&next=http://localhost/burkinafasofbtab/template.html" > Install </a>
 			</div>
-			
-			<div class="investgallery">
-				<div class="gallerycell">
-					<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg1.jpg"></a></div>					
-					<div class="cell2">
-						Animal Husbandary
-					</div>		
-				</div>
-
-				<div class="gallerycell">
-					<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg2.jpg"></a></div>					
-					<div class="cell2">
-						Agriculture
-					</div>		
-				</div>
-
-				<div class="gallerycell">
-					<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg4.jpg"></a></div>					
-					<div class="cell2">
-						Quarrying
-					</div>		
-				</div>
-				
-				<div class="gallerycell">
-					<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg3.jpg"></a></div>					
-					<div class="cell2">
-						Gold Mining
-					</div>		
-				</div>
-
-				<div class="gallerycell">
-					<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg5.jpg"></a></div>					
-					<div class="cell2">
-						Real Estate
-					</div>		
-				</div>
-
-				<div class="gallerycell">
-					<div class="cell1"><a href="http://www.burkinafasoindia.org"><img src="investimg6.jpg"></a></div>					
-					<div class="cell2">
-						Industry
-					</div>		
-				</div>
-			</div>
-			<div style="clear:both;"></div>			
-			
-			<div style="background: #006637;height: 31px;">
-				<p style="float: right;font-size: 19px;color: white;margin-right: 7px;">Designed by Ramanan Corporation</p>
-			</div>
-			<a href = "http://www.facebook.com/dialog/pagetab?app_id=397865700321865&next=http://localhost/burkinafasofbtab/template.html" > Install </a>
-		
 		</div>
+
+		<script type="text/javascript">
+		   window.fbAsyncInit = function() { 
+		        FB.init({
+		        	appId: '397865700321865', 
+		        	status: true, 
+		        	cookie: true, 
+		        	xfbml: true, 
+		        	oauth: true
+		       	});
+		    window.setTimeout(function() { 
+		        FB.Canvas.setAutoGrow(); }, 250); 
+		    }; 
+		     
+		    (function() { var e = document.createElement('script'); 
+		    e.async = true; 
+		    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js'; 
+		    document.getElementById('fb-root').appendChild(e); }()); 
+		</script>
+
 
 		<script type="text/javascript" src="build/dojo/dojo.js" data-dojo-config="async: 1, isDebug: 0"></script>
 		<script type="text/javascript">
